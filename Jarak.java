6c2d53a33fa49c92cee76a81e85265623af61f4f
Evinuria <63250961+Evinuria@users.noreply.github.com>
2020-04-28 15:10:05 +0700

    Add files via upload

--- Jarak.java
+++ Jarak.java
@@ -2,13 +2,10 @@ public class Jarak{
     int JP;
     int JS;
     int Skala;
-    
     public void jarak(){
-        if(Skala<=0){System.out.println("Angka yang anda masukkan salah");
-        }
+        if(Skala<=0){System.out.println("Angka yang anda masukkan salah");}
         else{
         JP= JS/Skala;
-        System.out.println("Jarak Pada Peta adalah = " + JP + " cm");
- }
+        System.out.println("Jarak Pada Peta adalah = " + JP + " cm");}
     }
 }
